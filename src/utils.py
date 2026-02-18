543ba1fa78dbd73e019ab79b8b2eab926ef1be8a
Alejacks <alejandrolopez2048@gmail.com>
2026-02-18 21:18:32 +0100

    feat: Changed __all__ position in utils.py and created a new method for creating a path if it doesn't exist and returning a boolean with the result

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import tomllib
 from pathlib import Path
 from typing import Any, Collection
 
+__all__ = ["read_toml_file", "needle_on_haystack", "needle_on_barn"]
+
 
 def read_toml_file(file_path: str | Path) -> dict:
     if isinstance(file_path, str):
@@ -14,9 +16,18 @@ def read_toml_file(file_path: str | Path) -> dict:
 def needle_on_haystack(needle: Any, haystack: Collection[Any]) -> bool:
     return needle in haystack
 
+
 def needle_on_barn(needle: Any, haystack: Any, barn: dict[Any, Any]) -> bool:
     if haystack in barn:
         return needle in barn[haystack]
     return False
 
-__all__ = ["read_toml_file", "needle_on_haystack", "needle_on_barn"]
+
+def create_path_if_not_exists(path: Path) -> bool:
+    try:
+        if not path.exists():
+            path.parent.mkdir(parents=True, exist_ok=True)
+            path.touch(exist_ok=True)
+        return True
+    except PermissionError:
+        return False
