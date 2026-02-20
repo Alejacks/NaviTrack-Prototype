7e1ad221a5f8fc652f719efbfc7ce0e16329d395
Alejacks <alejandrolopez2048@gmail.com>
2026-02-20 10:45:21 +0100

    feat: Added constants for checking valid values and units conversions

--- config/config.py
+++ config/config.py
@@ -1,12 +1,13 @@
 '''
-What do we need to do? Read configurations from the configuration path constant (which is a TOML), parse it, and be able to access the configuration values from a class instance (which
+What do we need to do? Read configurations from the configuration path constant (which is a TOML), parse it, and be able to access the configuration values from a class instance.
 
 For config.py:
 
 - Singleton class with a configuration dictionary - config.py / It was decided that a module would be better than a singleton / Done
 - Constant with the path of the configuration file - config.py / Done
 - Methods to read the configuration values and parse them from a TOML into a dictionary - utils.py / Done
-- Methods to check if the configuration values are valid (and exceptions for errors in this case)- utils.py
+- Constants for valid values so validating the TOML file is easier - config.py / Done
+- Methods to check if the configuration values are valid (and exceptions for errors in this case) - config.py
 - Method to validate the configuration values and raise an exception if they are invalid - config.py
 - Method to validate the TOML has no duplicated values within the same layer - utils.py
 - Method to get the number of layers in each dictionary - utils.py
@@ -29,17 +30,47 @@ All this should be checked and validated before proceeding on the program execut
 '''
 
 from pathlib import Path
-from typing import Any
+from typing import Any, Literal
 
 from src import utils
 
 CONFIG_FILE_PATH: Path = Path(__file__).parent.parent / "config.toml"
 
-VALID_EXPIRATION_TIME_UNITS: tuple[str, ...] = ("ms", "s", "m", "h", "d")
+DB_SECTION = "db"
+NAVITRACK_SECTION = "navitrack"
+EXPIRATION_SECTION = "expiration"
 
-VALID_EXPIRATION_BEHAVIOR: tuple[str, ...] = ("shadow", "delete", "backup")
+DB_FILE_KEY = "db_file"
+ON_COLLIDE_KEY = "on_collide"
+
+EXPIRATION_TIME_KEY = "time"
+EXPIRATION_UNIT_KEY = "unit"
+EXPIRATION_BEHAVIOR_KEY = "behavior"
+
+FRECENCY_KEY = "frecency"
+
+ExpirationUnit = Literal["ms", "s", "m", "h", "d"]
+ExpirationBehavior = Literal["shadow", "delete", "backup"]
+OnCollideBehavior = Literal["overwrite", "rename", "error"]
 
 VALID_ON_DUPLICATE_BEHAVIOR: tuple[str, ...] = ("overwrite", "rename", "error")
+VALID_EXPIRATION_TIME_UNITS: tuple[ExpirationUnit, ...] = ("ms", "s", "m", "h", "d")
+VALID_EXPIRATION_BEHAVIOR: tuple[ExpirationBehavior, ...] = ("shadow", "delete", "backup")
+VALID_ON_COLLIDE_BEHAVIOR: tuple[OnCollideBehavior, ...] = ("overwrite", "rename", "error")
+
+DEFAULT_EXPIRATION_TIME_UNIT: ExpirationUnit = "m"
+DEFAULT_EXPIRATION_BEHAVIOR: ExpirationBehavior = "shadow"
+DEFAULT_ON_COLLIDE_BEHAVIOR: OnCollideBehavior = "overwrite"
+DEFAULT_FRECENCY_VALUE: bool = True
+
+UNIT_TO_MS: dict[ExpirationUnit, int] = {
+    "ms": 1,
+    "s": 1_000,
+    "m": 60_000,
+    "h": 3_600_000,
+    "d": 86_400_000,
+}
+
 
 
 class Config:
